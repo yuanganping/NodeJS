cdc418e3d142fe62f6e4f4169799f37df17f50d9
Tracyc3 <562720180@qq.com>
2020-11-12 17:25:28 +0800

    send notification

---
+++ daily-info.js
@@ -0,0 +1,27 @@
+const rp = require("request-promise");
+
+// Server酱SCKEY
+const push_key = process.env.PUSH_KEY;
+
+// 发送通知
+async function sendNotification() {
+  // 去除末尾的换行
+  let SCKEY = push_key.replace(/[\r\n]/g, "");
+
+  const text = "今日消息推送";
+  const desp = "下面j具体的消息";
+
+  const options = {
+    uri: `https://sc.ftqq.com/${SCKEY}.send`,
+    form: { text, desp },
+    json: true,
+    method: "POST",
+  };
+  let result = await rp.post(options);
+
+  console.log("消息发送完毕： ", result.errno);
+}
+
+function main() {}
+
+main();
